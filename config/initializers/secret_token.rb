3dc851f7a2e6ab24c3d6925de5d908b261991426
vagrant <mpgarate@gmail.com>
2014-04-05 16:25:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flyerpenguin::Application.config.secret_key_base = 'f0f8371ac8366ca8478cfddd72056151c353d88556ca71cabed913ecd369fd5171f083470d85b8b485f09b0fe3039b0c46b6936a6ef811b9671cfa1637490833'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flyerpenguin::Application.config.session_store :cookie_store, key: '_flyerpenguin_session'
